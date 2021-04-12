6918e6c8ffa47cd31dcd777fb46757cf4d98d1c0
Rajesh Rao <rajesh.rao.rs9@gmail.com>
2021-04-12 19:09:53 -0400

    update main file execution

--- main.m
+++ main.m
@@ -16,11 +16,11 @@ addpath([root_dir filesep 'Temp'])
 addpath([root_dir filesep 'Output'])  
  
 %% running project scripts in synchronous order 
-% run('date_reader.m')                                                        % Reads in updated bond and swap data
-% run('match_tips_treasuries.m')                                              % Match TIPS and Treasury Issues
-% run('cash_flow_dates.m')                                                    % Calculate coupon payment dates for Treasury 
-% run('match_strips_treasury.m')                                              % Match STRIPS issues to Treasury coupon payments
-% run('actual_cash_flow_dates.m')                                             % Calculate coupon payment for Synthetic Treasury 
-% run('inflation_seasonal_adjust.m')                                          % Fit ZCIS Curve from Market Data
-% run('compute_mispricing.m')                                                 % Calculate the mispricing 
-% run('gen_bps_mp_new.m')                                                     % Aggregate Mispricing (in basis points) 
+% run('data_reader.m')                   % Reads in updated bond and swap data
+% run('match_tips_treasuries.m')         % Match TIPS and Treasury Issues
+% run('cash_flow_dates.m')               % Calculate coupon payment dates for UST 
+% run('match_strips_treasury.m')         % Match STRIPS issues to Treasury coupon 
+% run('actual_cash_flow_dates.m')        % Calculate coupon payment for Synthetic  
+% run('inflation_seasonal_adjust.m')     % Fit ZCIS Curve from Market Data
+% run('compute_mispricing.m')            % Calculate the mispricing 
+% run('gen_bps_mp_new.m')                % Aggregate Mispricing (in basis points) 
